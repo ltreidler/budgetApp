1e0f9cdd40f68d5a116446fcbdf54e55754e8e86
Leah Treidler <leahtreidler@Leahs-MacBook-Pro.local>
2020-08-09 19:03:20 -0700

    Setup oauth with udemy

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require('express');
 
 const app = express();
 
+require('./routes/authRoutes')(app);
+require('./services/passport');
+
+app.get('/', (req,res) => {
+    res.send('Thanks!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
